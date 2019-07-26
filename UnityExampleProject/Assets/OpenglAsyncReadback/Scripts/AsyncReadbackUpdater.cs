10668acfee6d9d906179ae3dcaf4a77263ac4a6f
yrc <21751230@zju.edu.cn>
2019-07-26 19:35:14 +0800

    Async readback updater for automatically call update in Unity

---
+++ UnityExampleProject/Assets/OpenglAsyncReadback/Scripts/AsyncReadbackUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yangrc.OpenGLAsyncReadback {
+    /// <summary>
+    /// A helper class to trigger readback update every frame.
+    /// </summary>
+    [AddComponentMenu("")]
+    public class AsyncReadbackUpdater : MonoBehaviour {
+        public static AsyncReadbackUpdater instance;
+        private void Awake() {
+            instance = this;
+        }
+        void Update() {
+            OpenGLAsyncReadbackRequest.Update();
+        }
+    }
+}
